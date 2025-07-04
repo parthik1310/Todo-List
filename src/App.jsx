9a713a0271762b8b36856494fbf846e31ac52d98
parthik Dhakecha <dhakechaparthk@gmail.com>
2025-07-04 13:23:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { ReactMemo } from "./components/hooks/Memo/ReactMemo";
+import { Todo } from "../src/project/Todo/Todo";
+// import MemoParentComponent from "./components/hooks/Memo/UseMemo";
+
+
+export const App = () => {
+  return <>
+     {/* <ReactMemo /> */}
+     {/* <MemoParentComponent /> */}
+     <Todo />
+  </>
+};
+
+export default App;
